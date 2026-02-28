c95229b63a8282e02aa6e254ba649244c6c048d6
Tóth András <sba.andras.toth@gmail.com>
2026-02-28 18:01:23 +0100

    rename setup function to adapterLegacyDemo and remove setup code

--- adapter-elektruha.ino
+++ adapter-elektruha.ino
@@ -13,4 +13,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(touchRead(T3));
+  // a saját megfigylet touchRead-et kellene, a legalacsonyabb és legmagasabb értéket
+  int ledBrightnessValue = map(touchRead(T3), 0, 1000, 0, 255);
+  analogWrite(GPIO4, ledBrightnessValue);
+  delay(DELAY_TIME);
 }
